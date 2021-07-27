5f20f1d6e934ac353a97a1789ec0663d6b959461
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-27 22:11:33 +0500

    Delete bootstrap.js

--- HelloWorld/bootstrap.js
+++
@@ -1,2 +0,0 @@
-document.write('<link rel="stylesheet" type="text/css" href="https://saifkhaki.github.io/JS-Widgets/HelloWorld/style.css">');
-document.write('<script type="text/javascript" src="https://saifkhaki.github.io/JS-Widgets/HelloWorld/WidgetCreate.js"></script>');
