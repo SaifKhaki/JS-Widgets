40f2db3966e622dc16a46c9cf699afdf6412fa44
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-28 00:09:11 +0500

    Alert Box Widget
    Created a alert box widget

---
+++ Alert Box/create_widget.js
@@ -0,0 +1,7 @@
+﻿var widget_content = "";
+widget_content += ('<div id="widget" style="width:' + wWidth + ';Height:' + wHeight + ';" >');
+//<!-- WIDGET HTML CODE START -->
+widget_content += ('<div class="alert alert-' + wAlertType + '" role="alert">' + wText + '</div>');
+//<!---------------------------->
+widget_content += ('</div>');
+document.getElementById('widget_container').innerHTML = widget_content;
