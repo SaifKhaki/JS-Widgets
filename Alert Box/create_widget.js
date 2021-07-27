517188320a02283fae4f9147dfae44d44d0324cb
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-28 00:48:10 +0500

    Delete create_widget.js

--- Alert Box/create_widget.js
+++
@@ -1,7 +0,0 @@
-var widget_content = "";
-widget_content += ('<div id="widget" style="width:' + wWidth + ';" >');
-//<!-- WIDGET HTML CODE START -->
-widget_content += ('<div class="alert alert-' + wAlertType + '" role="alert">' + wText + '</div>');
-//<!---------------------------->
-widget_content += ('</div>');
-document.getElementById('widget_container').innerHTML = widget_content;
