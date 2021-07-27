7b30656be25399d95c05c82ad0a538723b643e8f
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-27 22:14:13 +0500

    All format files finalized

---
+++ Format/create_widget.js
@@ -0,0 +1,9 @@
+﻿var widget_content = "";
+widget_content += ('<center><div id="widget" style="width:' + wWidth + ';Height:' + wHeight + ';" >');
+//<!-- WIDGET HTML CODE START -->
+
+
+
+//<!---------------------------->
+widget_content += ('</div></center>');
+document.getElementById('widget_container').innerHTML = widget_content;
---
+++ Format/linker.js
@@ -0,0 +1,4 @@
+﻿//<!-- LINK CODE START -->
+document.write('<link rel="stylesheet" type="text/css" href="https://saifkhaki.github.io/JS-Widgets/HelloWorld/style.css">');
+document.write('<script type="text/javascript" src="https://saifkhaki.github.io/JS-Widgets/HelloWorld/create_widget.js"></script>');
+//<!--------------------->
