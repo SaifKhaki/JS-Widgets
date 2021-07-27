e26e05ec037c619fb5ec1f6cdc8cb80b0c9e1f58
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-27 20:43:01 +0500

    Update bootstrap.js

--- HelloWorld/bootstrap.js
+++ HelloWorld/bootstrap.js
@@ -1,3 +1,2 @@
-﻿
-document.write('<link rel="stylesheet" type="text/css" href="style.css">');
-document.write('<script type="text/javascript" src="WidgetCreate.js"></script>');
+document.write('<link rel="stylesheet" type="text/css" href="https://saifkhaki.github.io/JS-Widgets/HelloWorld/style.css">');
+document.write('<script type="text/javascript" src="https://saifkhaki.github.io/JS-Widgets/HelloWorld/WidgetCreate.js"></script>');
