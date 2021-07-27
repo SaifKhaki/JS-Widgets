5737abcc1ccf70e53520f43618b02791e43742a7
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-27 20:44:33 +0500

    Update WidgetCreate.js

--- HelloWorld/WidgetCreate.js
+++ HelloWorld/WidgetCreate.js
@@ -1,6 +1,6 @@
-﻿WidgetCallback([
+WidgetCallback([
     {
-        "image_url": "images/widget.gif",
+        "image_url": "https://saifkhaki.github.io/JS-Widgets/HelloWorld/images/widget.gif",
         "text": " World ",
         "description": " My First Widget",
         "id": "1"
@@ -10,7 +10,7 @@ function WidgetCallback(JSONobject) {
     var wHelloWorld = JSONobject[0];
     var wHTML = "";
 
-    wHTML += ('<center><div id="MyWidget" style="background-image:url(images/bg.png);width:' + wWidth + ';Height:' + wHeight + ';" >');
+    wHTML += ('<center><div id="MyWidget" style="background-image:url(https://saifkhaki.github.io/JS-Widgets/HelloWorld/images/bg.png);width:' + wWidth + ';Height:' + wHeight + ';" >');
     wHTML += ('<br><br><img border="0" width="221" height="82" src="' + wHelloWorld.image_url + '">');
     wHTML += ('<br><a target="_blank" href="' + wURL + '" style="font-size:x-large;text-decoration:none;color:' + wFColor + '">');
     wHTML += ( wTitle + ' ' + wHelloWorld.text + '</a><br>');
