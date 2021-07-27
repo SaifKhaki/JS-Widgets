9c70e416287207f75c94ffe92e06d88024e18fac
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-27 20:15:37 +0500

    Add files via upload

---
+++ HelloWorld/WidgetCreate.js
@@ -0,0 +1,20 @@
+﻿WidgetCallback([
+    {
+        "image_url": "images/widget.gif",
+        "text": " World ",
+        "description": " My First Widget",
+        "id": "1"
+    }
+]);
+function WidgetCallback(JSONobject) {
+    var wHelloWorld = JSONobject[0];
+    var wHTML = "";
+
+    wHTML += ('<center><div id="MyWidget" style="background-image:url(images/bg.png);width:' + wWidth + ';Height:' + wHeight + ';" >');
+    wHTML += ('<br><br><img border="0" width="221" height="82" src="' + wHelloWorld.image_url + '">');
+    wHTML += ('<br><a target="_blank" href="' + wURL + '" style="font-size:x-large;text-decoration:none;color:' + wFColor + '">');
+    wHTML += ( wTitle + ' ' + wHelloWorld.text + '</a><br>');
+    wHTML += ('</div></center>');
+
+    document.getElementById('myFirstWidget').innerHTML = wHTML;
+}
---
+++ HelloWorld/bootstrap.js
@@ -0,0 +1,3 @@
+﻿
+document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+document.write('<script type="text/javascript" src="WidgetCreate.js"></script>');
