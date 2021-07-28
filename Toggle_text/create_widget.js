ab007e6d459f017fa4e313d1c27a1c78a84f3374
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-28 16:21:28 +0500

    Update create_widget.js

--- Toggle_text/create_widget.js
+++ Toggle_text/create_widget.js
@@ -1,5 +1,5 @@
-﻿var widget_content = "";
-widget_content += ('<div id="widget" style="width:' + wWidth + '; background-color: transparent; padding:100px;" >');
+var widget_content = "";
+widget_content += ('<div id="widget" style="width:' + wWidth + '; background-color: #18191a;" >');
 
 //<!-- WIDGET HTML CODE START -->
 widget_content += ('<button type="button" class="btn btn-' + wType + '" style="border-radius: 100px;">' + wOriginalText + '</button>');
