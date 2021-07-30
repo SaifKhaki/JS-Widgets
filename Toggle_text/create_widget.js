d7f70a42fadba5f7291650ec71dc9006bba9bdb7
Saif Ullah Bin Khaki <saifbinkhaki@gmail.com>
2021-07-30 22:21:55 +0500

    Delete create_widget.js

--- Toggle_text/create_widget.js
+++
@@ -1,9 +0,0 @@
-var widget_content = "";
-widget_content += ('<div id="widget" style="width:' + wWidth + ';"');
-
-//<!-- WIDGET HTML CODE START -->
-widget_content += ('<button type="button" class="btn btn-' + wType + '" style="border-radius: 100px;">' + wOriginalText + '</button>');
-//<!---------------------------->
-
-widget_content += ('</div>');
-document.getElementById('widget_container').innerHTML = widget_content;
